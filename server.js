1ca940914f6fb255ca4c5dc2f488142f810d5bc7
Daniel Beilfuss <d.beilfuss@icloud.com>
2023-11-18 11:12:45 -0700

    changed express.static directory

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 80;
 
 // Serve static files from the 'public' directory
-app.use(express.static("/public"));
+app.use(express.static("public"));
 
 app.listen(port, "0.0.0.0", () => {
   console.log(`Server running at http://localhost:${port}/`);
