6d21fc656ddcc4dbd50244e92f0edb9aa9b13654
Daniel Beilfuss <d.beilfuss@icloud.com>
2023-12-18 18:07:12 -0700

    commented out rollbar for troubleshooting

--- server.js
+++ server.js
@@ -2,6 +2,17 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+// include and initialize the rollbar library with your access token
+// var Rollbar = require("rollbar");
+// var rollbar = new Rollbar({
+//   accessToken: "8abf7cdc6a8740d5b076c52dc32c8bc7",
+//   captureUncaught: true,
+//   captureUnhandledRejections: true,
+// });
+
+// record a generic message and send it to Rollbar
+// rollbar.log("Hello world!");
+
 // Serve static files from the 'public' directory
 app.use(express.static("public"));
 
